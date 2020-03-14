29d4407af3469d91ff63eeedab04064a9bd74832
Leo <thinkabit.ukim@gmail.com>
2020-03-14 04:26:23 -0300

    feat(mkmr): rewrite with pathlib with automatic creation
    find_config will now almost always find a configuration, as it no longer
    relies on the user to create the configuration and instead creates the
    configuration file itself
    
    with the changes in mkmr/mkmr.py it will also write out the section and
    the url option in the configuration file, and if the user has passed
    --token it will write private_token as well

--- mkmr/utils.py
+++ mkmr/utils.py
@@ -1,8 +1,9 @@
 def find_config(p):
-    from os import path, getenv
+    from pathlib import Path
+    from os import getenv
 
     if p is not None:
-        if path.isfile(p):
+        if p.is_file():
             return p
         else:
             raise ValueError("couldn't find configuration file in {}".format(
@@ -10,12 +11,29 @@ def find_config(p):
 
     xdgpath = getenv("XDG_CONFIG_HOME")
     if xdgpath is not None:
-        xdgpath = path.join(xdgpath, 'mkmr/config')
-        if path.isfile(xdgpath):
-            return xdgpath
-        else:
-            raise ValueError("couldn't find configuration in {}".format(
-                             xdgpath))
+        xdgpath = Path(xdgpath)
+        xdgpath = xdgpath / 'mkmr' / 'config'
+
+        # Get the parent
+        p = xdgpath.parent
+
+        # If the parent exists and is not a directory
+        # remove it, we need it to be a directory
+        if p.exists() and not p.is_dir():
+            p.unlink()
+
+        # Create parent directions of the parent directory
+        p.mkdir(parents=True, exist_ok=True)
+
+        # If the file exists but is not a file then remove
+        # it is as well
+        if xdgpath.exists() and not xdgpath.is_file():
+            xdgpath.unlink()
+
+        # Create it with nice permissions for a file that
+        # hold secrets
+        xdgpath.touch(mode=0o600)
+        return xdgpath
 
     homepath = getenv("HOME")
     if homepath is None:
@@ -24,9 +42,26 @@ def find_config(p):
                          "config relative to it")
 
     if xdgpath is None:
-        xdgpath = path.join(homepath, '.config/mkmr/config')
-        if path.isfile(xdgpath):
-            return xdgpath
-        else:
-            raise ValueError("couldn't find configuration file in {}".format(
-                             xdgpath))
+        xdgpath = Path(homepath)
+        xdgpath = xdgpath / '.config' / 'mkmr' / 'config'
+
+        # Get the parent
+        p = xdgpath.parent
+
+        # If the parent exists and is not a directory
+        # remove it, we need it to be a directory
+        if p.exists() and not p.is_dir():
+            p.unlink()
+
+        # Create parent directions of the parent directory
+        p.mkdir(parents=True, exist_ok=True)
+
+        # If the file exists but is not a file then remove
+        # it is as well
+        if xdgpath.exists() and not xdgpath.is_file():
+            xdgpath.unlink()
+
+        # Create it with nice permissions for a file that
+        # hold secrets
+        xdgpath.touch(mode=0o600)
+        return xdgpath
