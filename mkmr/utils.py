6a60520afbe134b3110088549d756e1a90fc9186
Leo <thinkabit.ukim@gmail.com>
2020-03-15 04:11:18 -0300

    refactor(utils): make find_config use create_file

--- mkmr/utils.py
+++ mkmr/utils.py
@@ -37,7 +37,7 @@ def find_config(p):
     xdgpath = getenv("XDG_CONFIG_HOME")
     if xdgpath is not None:
         xdgpath = Path(xdgpath)
-        return create_config(xdgpath / 'mkmr' / 'config')
+        return create_file(xdgpath / 'mkmr' / 'config')
 
     homepath = getenv("HOME")
     if homepath is None:
@@ -46,4 +46,4 @@ def find_config(p):
 
     if xdgpath is None:
         xdgpath = Path(homepath)
-        return create_config(xdgpath / '.config' / 'mkmr' / 'config')
+        return create_file(xdgpath / '.config' / 'mkmr' / 'config')
