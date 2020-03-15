560bbb1922df7217b361e8dcef658ca3921a4ddd
Leo <thinkabit.ukim@gmail.com>
2020-03-15 02:13:29 -0300

    refactor(utils): strip config creation into a separate function
    other places of the code like api might want to use it in the future for
    stuff like creating the cache directories

--- mkmr/utils.py
+++ mkmr/utils.py
@@ -1,5 +1,30 @@
+from pathlib import Path
+
+
+def create_config(xdgpath: Path) -> Path:
+    # Get the parent
+    p = xdgpath.parent
+
+    # If the parent exists and is not a directory
+    # remove it, we need it to be a directory
+    if p.exists() and not p.is_dir():
+        p.unlink()
+
+    # Create parent directions of the parent directory
+    p.mkdir(parents=True, exist_ok=True)
+
+    # If the file exists but is not a file then remove
+    # it is as well
+    if xdgpath.exists() and not xdgpath.is_file():
+        xdgpath.unlink()
+
+    # Create it with nice permissions for a file that
+    # hold secrets
+    xdgpath.touch(mode=0o600)
+    return xdgpath
+
+
 def find_config(p):
-    from pathlib import Path
     from os import getenv
 
     if p is not None:
@@ -12,56 +37,13 @@ def find_config(p):
     xdgpath = getenv("XDG_CONFIG_HOME")
     if xdgpath is not None:
         xdgpath = Path(xdgpath)
-        xdgpath = xdgpath / 'mkmr' / 'config'
-
-        # Get the parent
-        p = xdgpath.parent
-
-        # If the parent exists and is not a directory
-        # remove it, we need it to be a directory
-        if p.exists() and not p.is_dir():
-            p.unlink()
-
-        # Create parent directions of the parent directory
-        p.mkdir(parents=True, exist_ok=True)
-
-        # If the file exists but is not a file then remove
-        # it is as well
-        if xdgpath.exists() and not xdgpath.is_file():
-            xdgpath.unlink()
-
-        # Create it with nice permissions for a file that
-        # hold secrets
-        xdgpath.touch(mode=0o600)
-        return xdgpath
+        return create_config(xdgpath / 'mkmr' / 'config')
 
     homepath = getenv("HOME")
     if homepath is None:
         raise ValueError("Neither XDG_CONFIG_HOME or HOME are set, please "
-                         "set XDG_CONFIG_HOME and place the file in mkmr/"
-                         "config relative to it")
+                         "set XDG_CONFIG_HOME")
 
     if xdgpath is None:
         xdgpath = Path(homepath)
-        xdgpath = xdgpath / '.config' / 'mkmr' / 'config'
-
-        # Get the parent
-        p = xdgpath.parent
-
-        # If the parent exists and is not a directory
-        # remove it, we need it to be a directory
-        if p.exists() and not p.is_dir():
-            p.unlink()
-
-        # Create parent directions of the parent directory
-        p.mkdir(parents=True, exist_ok=True)
-
-        # If the file exists but is not a file then remove
-        # it is as well
-        if xdgpath.exists() and not xdgpath.is_file():
-            xdgpath.unlink()
-
-        # Create it with nice permissions for a file that
-        # hold secrets
-        xdgpath.touch(mode=0o600)
-        return xdgpath
+        return create_config(xdgpath / '.config' / 'mkmr' / 'config')
